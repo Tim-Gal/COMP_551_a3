57f4d626ae5dc014ab7387e69ad285f71d0ff7b9
Tim <timoxa.gal@gmail.com>
2023-11-12 18:40:20 -0500

    preprocess the huggingface data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datasets import load_dataset
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 dataset = load_dataset("dair-ai/emotion")
 
@@ -6,4 +8,16 @@ train_dataset = dataset['train']
 validation_dataset = dataset['validation']
 test_dataset = dataset['test']
 
-print(train_dataset[0])
+"""
+with open("text.txt", 'w') as file:
+    for line in train_dataset['text']:
+        file.write(line + '\n')
+"""
+
+vectorizer = CountVectorizer()
+X_train_vectorized = vectorizer.fit_transform(train_dataset['text'])
+
+print(X_train_vectorized.toarray())
+print("Vocabulary:", vectorizer.get_feature_names_out())
+
+
